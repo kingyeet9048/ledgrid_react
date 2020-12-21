e93cfd139a95063b7976fb2dea6ce68dcc4fcd73
Sulaiman Bada <sulaimanbada8048@gmail.com>
2020-12-21 01:46:02 -0600

    Working on the login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import LoginPerson from './../../public/login_person.png';
-import './../styles/_Login.scss';
+import '../styles/_Login.scss';
 
 class Login extends Component {
     render() {
         return(
             <div id="LoginContainer">
+                <header class="center" >WSU CS Project - LedGrid</header>
                 <img src={LoginPerson} className="profile" alt="profile" />          
             </div>
         );
